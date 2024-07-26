776026d8cce4c7a288dd294e1b904daa9e574549
Ryan <ryan.abolfazl@gmail.com>
2024-07-26 19:25:18 +0330

    serializers

---
+++ product/serializers.py
@@ -0,0 +1,20 @@
+from .models import Brand, Category, Product
+from rest_framework import serializers
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+
+class BrandSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Brand
+        fields = '__all__'
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
